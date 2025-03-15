ba595e54d175f97bbc3065ba234f9ce37c71dc12
laurapet <laura.peter258@gmail.com>
2025-03-15 17:10:10 +0100

    feat: icon

---
+++ src/routes/components/footer.svelte
@@ -0,0 +1,25 @@
+<script>
+    import IconButton from './IconButton.svelte';
+</script>
+
+<div class="w-full">
+	<div class="flex mx-22 mt-22 mb-8 justify-between">
+	    <div class="flex items-center gap-2">
+	        <IconButton icon="mail"/>
+	        <IconButton icon="github"/>
+	        <IconButton icon="linkedin"/>
+	        <IconButton icon="coffee"/>
+	    </div>
+	    <div class="grid grid-cols-1 text-right justify-items-stretch">
+	    	<a href="#" class="underline text-portfolio-white justify-self-end hover:bg-portfolio-blue hover:text-portfolio-white">Imprint</a>
+	    	<p class="text-portfolio-white">
+	    		© Laura Peter, 2025
+	    	</p>
+	    </div>
+	</div>
+</div>
+<style scoped>
+	a:hover {
+		color: var(--color-portfolio-white);
+	}
+</style>
